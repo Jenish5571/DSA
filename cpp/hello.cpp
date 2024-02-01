f158f54645a3fc970faf66608ef30cc3b9eeff0b
Jenish5571 <jenishhirapara89@gmail.com>
2024-02-01 21:35:48 +0530

    .

---
+++ cpp/hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+    int a = 10;
+    int b = 20;
+
+    cout << "Value of variable 'a' is : ";
+    cout << a ;
+
+    cout << "Value of variable 'b' is : ";
+    cout << b ;
+
+}
